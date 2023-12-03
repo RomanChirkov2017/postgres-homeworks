6c569d722e7e49339f68141d00c66d5c84f4b53b
RomanChirkov2017 <138663746+RomanChirkov2017@users.noreply.github.com>
2023-12-03 22:31:08 +1000

    Добавлен скрипт в main.py и добавлены SQL-команды в create_tables.sql

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,43 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+current_directory = os.getcwd()
+
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='Mmclegnum157',
+    options='-c search_path=public',
+    client_encoding='UTF8'
+)
+
+cur = conn.cursor()
+with open(current_directory + '\\north_data\\' + "customers_data.csv", 'r') as file:
+    next(file)
+    reader = csv.reader(file)
+    for row in reader:
+        cur.execute('INSERT INTO customers (customer_id, company_name, contact_name)'
+                    'VALUES (%s, %s, %s)', (row[0], row[1], row[2]))
+
+with open(current_directory + '\\north_data\\' + "employees_data.csv", 'r') as file:
+    next(file)
+    reader = csv.reader(file)
+    for row in reader:
+        cur.execute('INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes)'
+                    'VALUES (%s, %s, %s, %s, %s, %s)', (row[0], row[1], row[2], row[3], row[4], row[5]))
+
+with open(current_directory + '\\north_data\\' + "orders_data.csv", 'r') as file:
+    next(file)
+    reader = csv.reader(file)
+    for row in reader:
+        cur.execute('INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city)'
+                    'VALUES (%s, %s, %s, %s, %s)', (row[0], row[1], row[2], row[3], row[4]))
+
+conn.commit()
+cur.close()
+conn.close()
+
